851336f4a85e2adb47ea618a23b2c8b1127c3d8c
gift-a <ksenia.gift@gmail.com>
2018-03-06 23:06:24 +0200

    add layout grid for form

--- src/components/add-transaction-dialog/add-transaction-dialog.js
+++ src/components/add-transaction-dialog/add-transaction-dialog.js
@@ -1,4 +1,20 @@
 import template from "./add-transaction-dialog.html";
+import button from "./add-transaction-button.html";
+// import { MDCSelect } from "@material/select/index";
+
+// console.log(MDCSelect);
+
+// const select = new MDCSelect(document.querySelector(".mdc-select1"));
+
+// console.log(select);
+
+// select.listen("MDCSelect:change", () => {
+//   alert(
+//     `Selected "${select.selectedOptions[0].textContent}" at index ${
+//       select.selectedIndex
+//     } ` + `with value "${select.value}"`
+//   );
+// });
 
 export class AddTransactionComponent {
   constructor(mountPoint) {
