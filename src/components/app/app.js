b4cd8a9f5b367432796ec68a438ec0e3e2f1c84a
Vladyslav <34074565+Vladyslav-Plakhuta@users.noreply.github.com>
2018-03-08 12:35:17 +0200

    updated

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,18 @@
 import template from "./app.html";
+import { BuxyTableTransactionsComponent } from "../buxy-table-transactions/buxy-table-transactions";
 
 export class AppComponent {
   constructor(mountPoint) {
     this.mountPoint = mountPoint;
   }
 
+  querySelectors() {
+     this.BuxyTableTransactionsMountPoint = this.mountPoint.querySelector(
+      ".app__buxy-table-transactions"
+    );
+      
   mount() {
     this.mountPoint.innerHTML = template({ name: "Ihor" });
+    this.querySelectors();
   }
 }
