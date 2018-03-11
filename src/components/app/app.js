e97cd6f4072e9a2ec9fa7fcfca1e9f326ba7c063
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-03-11 16:26:43 +0200

    popup

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import template from "./app.html";
 import { DrawerComponent } from "../drawer/drawer";
 import { ToolbarComponent } from "../toolbar/toolbar";
+import { PopupComponent } from "../popup/popup";
 
 export class AppComponent {
   constructor(mountPoint) {
@@ -12,6 +13,8 @@ export class AppComponent {
       ".app__container-toolbar-point"
     );
     this.drawerMountPoint = this.mountPoint.querySelector(".app__drawer-point");
+    this.menuButton = document.querySelector("#menu-button");
+    this.popupMountPoint = document.querySelector(".popup_mount_point");
   }
 
   mountChildren() {
@@ -21,6 +24,11 @@ export class AppComponent {
     this.toolBarComponent.mount();
     this.drawerComponent = new DrawerComponent(this.drawerMountPoint);
     this.drawerComponent.mount();
+    this.popup = new PopupComponent(this.popupMountPoint);
+    this.popup.mount();
+    this.menuButton.addEventListener("click", () => {
+      this.popup.toggle();
+    });
   }
 
   handleToolbarMenuClick() {
