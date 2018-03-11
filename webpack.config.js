185032b0a5256a35f0cb0be518539b71406753ee
gift-a <ksenia.gift@gmail.com>
2018-03-11 17:14:32 +0200

    move style imports as css to index.scss, reestablish webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -20,27 +20,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              importer: function(url, prev) {
-                if (url.indexOf("@material") === 0) {
-                  var filePath = url.split("@material")[1];
-                  var nodeModulePath = `./node_modules/@material/${filePath}`;
-                  return { file: require("path").resolve(nodeModulePath) };
-                }
-                return { file: url };
-              }
-            }
-          }
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   }
