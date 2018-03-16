0b8cc58f732eb8474e753e7a539347098b7406a0
gift-a <ksenia.gift@gmail.com>
2018-03-16 10:03:27 +0200

    add service currency converter

---
+++ src/services/currency-converter-uah.js
@@ -0,0 +1,16 @@
+export class CurrencyConverterUAH {
+  static get(curr, amount) {
+    switch (curr) {
+      case "USD":
+        amount *= 27;
+        break;
+      case "EUR":
+        amount *= 33;
+        break;
+      default:
+        console.error(`convertor. no such currency: ${curr}`);
+        return 0;
+    }
+    return amount;
+  }
+}
