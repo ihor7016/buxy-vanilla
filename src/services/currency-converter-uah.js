c0fbc28cbbba23e85db6e1c58d77c4e365122048
gift-a <ksenia.gift@gmail.com>
2018-03-17 15:43:21 +0200

    change switch to if-else in converter

--- src/services/currency-converter-uah.js
+++ src/services/currency-converter-uah.js
@@ -1,16 +1,13 @@
 export class CurrencyConverterUAH {
   static convert(curr, amount) {
     let uah;
-    switch (curr) {
-      case "USD":
-        uah = amount * 27;
-        break;
-      case "EUR":
-        uah = amount * 33;
-        break;
-      default:
-        console.error(`converter. no such currency: ${curr}`);
-        return 0;
+    if (curr == "USD") {
+      uah = amount * 27;
+    } else if (curr == "EUR") {
+      uah = amount * 33;
+    } else {
+      console.error(`converter. no such currency: ${curr}`);
+      uah = 0;
     }
     return uah;
   }
