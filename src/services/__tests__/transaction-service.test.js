2b581e8e49e353c9486d6db521c916a7fbe8f1e0
gift-a <ksenia.gift@gmail.com>
2018-03-26 13:53:58 +0300

    add describe it

--- src/services/__tests__/transaction-service.test.js
+++ src/services/__tests__/transaction-service.test.js
@@ -69,36 +69,46 @@ describe("Transactions storage service", () => {
     StorageService.set.mockImplementation(() => Promise.resolve());
   });
 
-  test("get should return array of objects", () => {
-    return TransactionListService.get().then(arr => {
-      expect(arr).toEqual(sample);
-      expect(StorageService.get).toHaveBeenCalledWith(key);
+  describe("get method", () => {
+    it("should return array of objects", () => {
+      return TransactionListService.get().then(arr => {
+        expect(arr).toEqual(sample);
+        expect(StorageService.get).toHaveBeenCalledWith(key);
+      });
     });
   });
 
-  test("set should send correct data", () => {
-    return TransactionListService.set(sample).then(() => {
-      expect(StorageService.set).toHaveBeenCalledWith(key, sample);
+  describe("set method", () => {
+    it("should send correct data", () => {
+      return TransactionListService.set(sample).then(() => {
+        expect(StorageService.set).toHaveBeenCalledWith(key, sample);
+      });
     });
   });
 
-  test("add should add, send and return correct data", () => {
-    return TransactionListService.add(dataToAdd).then(arr => {
-      expect(StorageService.set).toHaveBeenCalledWith(key, addRes);
-      expect(arr).toEqual(addRes);
+  describe("add method", () => {
+    it("should add, send and return correct data", () => {
+      return TransactionListService.add(dataToAdd).then(arr => {
+        expect(StorageService.set).toHaveBeenCalledWith(key, addRes);
+        expect(arr).toEqual(addRes);
+      });
     });
   });
 
-  test("del should del, send and return correct data", () => {
-    return TransactionListService.del(transId).then(arr => {
-      expect(StorageService.set).toHaveBeenCalledWith(key, delRes);
-      expect(arr).toEqual(delRes);
+  describe("del method", () => {
+    it("should del, send and return correct data", () => {
+      return TransactionListService.del(transId).then(arr => {
+        expect(StorageService.set).toHaveBeenCalledWith(key, delRes);
+        expect(arr).toEqual(delRes);
+      });
     });
   });
 
-  test("deleteByAccountId should del and send correct data", () => {
-    return TransactionListService.deleteByAccountId(accId).then(arr => {
-      expect(StorageService.set).toHaveBeenCalledWith(key, delRes);
+  describe("deleteByAccountId method", () => {
+    it(" should del and send correct data", () => {
+      return TransactionListService.deleteByAccountId(accId).then(arr => {
+        expect(StorageService.set).toHaveBeenCalledWith(key, delRes);
+      });
     });
   });
 });
