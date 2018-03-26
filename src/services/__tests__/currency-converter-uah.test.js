70b888a09519712f73afc181a9c73621cc1c0334
gift-a <ksenia.gift@gmail.com>
2018-03-26 14:12:47 +0300

    cover 100 currency

--- src/services/__tests__/currency-converter-uah.test.js
+++ src/services/__tests__/currency-converter-uah.test.js
@@ -1,15 +1,21 @@
 import { CurrencyConverterUAHService } from "../currency-converter-uah";
 
 describe("converts in UAH", () => {
-  test("10 EUR equals 330 UAH", () => {
+  it("should convert 10 EUR in 330 UAH", () => {
     const curr = "EUR";
     const amount = 10;
     expect(CurrencyConverterUAHService.convert(curr, amount)).toBe(330);
   });
 
-  test("10 USD equals 270 UAH", () => {
+  it("should convert 10 USD in 270 UAH", () => {
     const curr = "USD";
     const amount = 10;
     expect(CurrencyConverterUAHService.convert(curr, amount)).toBe(270);
+  });
+
+  it("should return 0 with UAH", () => {
+    const curr = "UAH";
+    const amount = 10;
+    expect(CurrencyConverterUAHService.convert(curr, amount)).toBe(0);
   });
 });
