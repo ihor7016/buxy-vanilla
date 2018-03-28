907bc2b7948b95c2b5eb1a23ba8961e579876811
gift-a <ksenia.gift@gmail.com>
2018-03-28 11:28:18 +0300

    mock math.random

--- src/services/__tests__/color-generator.spec.js
+++ src/services/__tests__/color-generator.spec.js
@@ -1,7 +1,11 @@
 import { ColorGeneratorService } from "../color-generator";
 
+beforeAll(() => {
+  Math.random = jest.fn(() => 0.5);
+});
+
 describe("ColorGeneratorService", () => {
-  it("should return number #d{6}", () => {
+  it("should return rgb", () => {
     expect(ColorGeneratorService.get()).toMatch(/^#[a-f0-9]{6}$/);
   });
 });
